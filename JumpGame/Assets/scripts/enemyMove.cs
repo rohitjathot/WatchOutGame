59ed17ae5c96867c404e926024f72514e65d4a9a
Rohit Jathot <rohitjaghot7@gmail.com>
2017-06-09 14:10:00 +0530

    First Commit
    Uploaded WatchOut Game

---
+++ JumpGame/Assets/scripts/enemyMove.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMove : MonoBehaviour {
+    
+    public float speed = 0.5f;
+    public GameObject[] enemy;
+    public float frequency=0.5f;
+    float counter = 0.0f;
+    public Transform challangeSpawnPoint;
+
+	// Use this for initialization
+	void Start () {
+        GenerateRandmChallenge();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (counter <= 0.0)
+        {
+            GenerateRandmChallenge();
+        }else
+        {
+            counter -= Time.deltaTime * frequency;
+        }
+
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            moveLeft(transform.GetChild(i).gameObject);
+        }
+
+	}
+
+    void moveLeft(GameObject gameObject)
+    {
+        gameObject.transform.position  -= Vector3.right * speed * Time.deltaTime;
+        if (gameObject.transform.position.x < -12)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void GenerateRandmChallenge()
+    {
+        GameObject newEnemy = Instantiate(enemy[Random.Range(0, enemy.Length)], challangeSpawnPoint.position, Quaternion.identity) as GameObject;
+        newEnemy.transform.parent = transform;
+        counter = 1.0f;
+    }
+
+}
---
+++ JumpGame/Assets/scripts/playerJump.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerJump : MonoBehaviour {
+
+    public float jumpForce = 0.5f;
+    public GameObject panel;
+    Rigidbody2D myRigidBody;
+    bool isGrounded = false;
+    float postion=0.0f;
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = transform.GetComponent<Rigidbody2D>();
+        postion = transform.position.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if(isGrounded)
+            myRigidBody.AddForce(Vector3.up * jumpForce * myRigidBody.mass * myRigidBody.gravityScale * 20.0f);
+        }
+
+        if(transform.position.x < postion)
+        {
+            GameOver();
+        }
+
+	}
+
+   public void GameOver()
+    {
+
+        //over.SetGameOver();
+        panel.SetActive(true);
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "stage")
+        {
+            isGrounded=true;
+        }
+        if (collision.collider.tag == "spike")
+        {
+            GameOver();
+        }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "stage")
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "stage")
+        {
+            isGrounded = true;
+        }
+    }
+
+}
